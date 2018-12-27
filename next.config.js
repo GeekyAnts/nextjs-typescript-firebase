6610e11e537138834d88fed7c1fc25c7a95db2a4
Neeraj Sinha <neerajs@geekyants.com>
2018-12-27 12:03:15 +0530

    basic setup

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withTypescript = require('@zeit/next-typescript');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withTypescript(
+  withSass({
+    webpack(config, options) {
+      return config;
+    },
+    cssModules: true,
+    sassLoaderOptions: {}
+  })
+);
