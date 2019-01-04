d9d826e2a9cbe7d791d23f9b2fd6fe5ea65d2744
Neeraj Sinha <neerajs@geekyants.com>
2019-01-04 14:24:14 +0530

    added link to reset password in nav bar

--- pages/reset-password.tsx
+++ pages/reset-password.tsx
@@ -5,15 +5,19 @@ import ResetPassword from '../src/components/Auth/ResetPassword';
 import Link from 'next/link';
 import * as styles from '../styles/main.scss';
 import CheckAuth from '../src/components/Auth/CheckAuth';
+import NavBar from '../src/components/Navigation/NavBar';
 
 export default class extends React.Component {
   render() {
     return (
-      <div
-        className={[styles.container, styles['centered-container']].join(' ')}
-      >
-        <CheckAuth />
-        <ResetPassword />
+      <div>
+        <NavBar />
+        <div
+          className={[styles.container, styles['centered-container']].join(' ')}
+        >
+          <CheckAuth />
+          <ResetPassword />
+        </div>
       </div>
     );
   }
